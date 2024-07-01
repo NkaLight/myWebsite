b39e35595ced182f5a7cef93ae8243744666e2dd
ndlnk218 <ndlnk218@student.otago.ac.nz>
2024-07-02 06:53:24 +1200

    removing unused vars

--- src/components/Pages/About.js
+++ src/components/Pages/About.js
@@ -1,13 +1,7 @@
 import React from "react";
-import Typewriter from "typewriter-effect";
 
 
 function About(props){
-
-    const aboutMe = `Well I am a software enthusiast who also loves going out to themed parties. I am a huge lover of <strong>skateboarding too!</strong><br/>I initially fell in love with the
-    adrenaline from skateboarding whilst I was in <strong>South Africa</strong> before I came to Otago for my Uni studies. My time here in <strong>New Zealand</strong> has been nothing short of amazing.<br/>
-    As I work on my degree I wish to have a more holistic understanding of programming and computing in general uitilizing and mastering many tools to solve real world problem. 
-    `
 
     return(
         <div className="projects-container" style={props.darkMode ? {color:"white"} : {color:"black"}}>
