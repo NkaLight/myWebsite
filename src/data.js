6a9167131f19579e9ad86168ab820913ce81c4e2
Nkanyiso <ndlnk218@student.otago.ac.nz>
2024-11-28 16:55:12 +1300

    appended projects data

---
+++ src/data.js
@@ -0,0 +1,37 @@
+const projects = [
+    {
+        id: 1,
+        name: "Portfolio Website",
+        description: "A personal portfolio website to showcase my projects and skills.",
+        detailedDescription:"More things to explain here.",
+        technologies: ["HTML", "CSS", "JavaScript", "React"],
+        gitHub: "https://github.com/NkaLight/store-system",
+        skillDescription: "Modern Client-side Web development",
+        startDate: "2024-01-10",
+        endDate: "2024-02-15",
+    },
+    {
+        id: 2,
+        name: "Tech store",
+        description: "An e-commerce platform for small businesses to sell products online.",
+        detailedDescription:"More things to explain here.",
+        technologies: ["Node.js", "Express", "MySQL", "Vue.js", "Java"],
+        gitHub: "https://github.com/NkaLight/store-system",
+        skillDescription: "Server-side web-development,Implementing the Middleware and implementing a MySQL database to store my data, to fullfill functional requirements, based on my information gathering with an arbitary client, as part of my INFO202 assignment. The more technical aspects I expanded was unit testing, and the importance of good code coverage.",
+        startDate: "2024-03-01",
+        endDate: "2024-06-30",
+    },
+    {
+        id: 3,
+        name: "A non-destructive image editor",
+        description: "A Java based applicaton, for editing images",
+        detailedDescription:"More things to explain here.",
+        technologies: ["Flutter", "Dart", "Firebase"],
+        gitHub: "https://github.com/NkaLight/Project-Andie",
+        skillDescription: "Learn't about the importance of working in a team, where we would delegate work very early on every week, and work on our specific project tasks for the week. I also learnt about the importance of asking for help and how efficient software development becomes in a collaborative dev team.",
+        startDate: "2024-07-15",
+        endDate: "2024-09-10",
+    },
+];
+
+export default projects;
