3e3090a98bdd39ea317c30e6bb9e66d2becd44ac
Nkanyiso <ndlnk218@student.otago.ac.nz>
2024-11-28 18:09:54 +1300

    project list v1 complete

--- src/components/PCards.jsx
+++ src/components/PCards.jsx
@@ -2,10 +2,28 @@ import React from "react";
 
 
 const PCards = (props)=>{
-    console.log(props.name)
     return(
         <div className="card">
-            {props.name}
+            <section className="card-upper-section">
+                <div className="card-title"><strong>{props.name}</strong></div>
+                <div className="card-descr">{props.description}</div>
+            </section>
+            <section className="card-main-section">
+                <p><strong>Skills & Technologies</strong></p>
+                <ul className="card-skill-list">
+                    {props.technologies.map((skill) => {
+                        return <li>{skill}</li>
+                    })}
+                </ul>
+            </section>
+            <section className="card-lower-section">
+                {
+                    <a className={props.darkMode ? "main-btns-darkmode" : "main-btns-lightMode"}>
+                        {props.name.includes("course") ? "HackTheBox" : "GitHub" }
+                    </a>
+                }
+            </section>
+            
         </div>
     )
 }
